f50f8b4f8f9f4eca66af868eaa0af3e45d9a5ace
Anne <anneris.wong@gmail.com>
2024-07-28 01:28:03 -0400

    Temperature cutom pipe

---
+++ src/app/temperature.pipe.ts
@@ -0,0 +1,39 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temperature',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ): string {
+    let val: number;
+    let outputTemp: number;
+    let symbol: '°C' | '°F';
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    }
+
+    return `${outputTemp} ${symbol}`;
+  }
+}
